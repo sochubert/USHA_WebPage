0685cb1f408836470e737471022e31d9dfa70b69
sochubert <sochubert@gmail.com>
2022-01-10 15:50:44 +0900

    bug fix

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ const Home = () => {
         <div id="home">
             <div className='home_text_box'>
                 <h1>좋아하는 것을</h1>
-                <h1>잘하는 것으로, 으샤 USHA!</h1>
+                <h1>잘하는 것으로, <span className="responsive">으샤 USHA!</span></h1>
             </div>
         </div>
     );
