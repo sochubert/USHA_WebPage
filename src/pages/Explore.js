9808c234c170bea76b99ffa8db34a499dd7e49d6
sochubert <sochubert@gmail.com>
2022-04-18 18:10:03 +0900

    AWS error catch

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,22 +1,8 @@
 import React from "react";
-import { Button, Pane, Text, majorScale, Combobox, AirplaneIcon } from 'evergreen-ui';
 
 function Explore() {
     return (
-        <Pane display="flex" alignItems="center" marginX={majorScale(2)}>
-            <Button>Click me!</Button>
-            <Text>This is a clickable Button</Text>
-            <Combobox
-                items={['Banana', 'Orange', 'Apple', 'Mango']}
-                onChange={selected => console.log(selected)}
-                placeholder="Fruit"
-                autocompleteProps={{
-                    // Used for the title in the autocomplete.
-                    title: 'Fruit'
-                }}
-            />
-            <AirplaneIcon />
-        </Pane>
+        <h1>Test</h1>
     )
 }
 
